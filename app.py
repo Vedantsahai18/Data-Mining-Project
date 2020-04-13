834cb459fb633c21fc2acd6b8470f7679f173ed5
vedantsahai18 <vedantsahai18@gmail.com>
2020-04-13 17:57:22 +0530

    basic website created

--- app.py
+++ app.py
@@ -1,64 +1,60 @@
+import os
 
-from modules.kmeans import kmeans
-#from modules.gauss import gauss
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-import os.path
-#from flask_bootstrap import Bootstrap 
-import pandas as pd 
-import numpy as np 
+from flask import Flask, request, flash, redirect, render_template, url_for
+from werkzeug.utils import secure_filename
 
-# ML Packages
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.externals import joblib
+from kmeans import elbow, parse, kmeans
 
-exts = ['csv', 'json']
+UPLOAD_FOLDER = 'csv'
+ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
-services = {
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-    'kmeans': kmeans
-    #'gauss': gauss,
 
-}
 
-cors = CORS(app, resources={
-    r'/{}'.format(service): {"origins": "*"} for service in services
-})
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET'])
-def test():
-    return render_template('upload.html')
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
+@app.route('/algo', methods = ['GET', 'POST'])
+def algo():
     if request.method == 'POST':
-        data = request.files['data']
-        #service_name=request.form['service']
-        #service_func = services[service_name]
-        ext = data.filename.split('.')[1]
-        if(ext in exts):
-            data.save('uploads/' + data.filename)
-            #file_path='uploads/'+data.filename
-            #output_data = service_func(file_path)
-            #return output_data
-        else:
-            return 'File type not accepted!'
+        filename = request.form['filename']
+        if request.form['algorithm'] == "DB-SCAN":
+            chosen_algo=request.form['algorithm']
+            
+            return render_template('result.html', image1 = 'elbow.png')
+        elif request.form['algorithm'] == "kmean":
+            X = parse(filename, 3, 4)
+            elbow(X)
+            kmeans(X, n_clusters=5)
+            text = open('static\summary.txt', 'r+')
+            content = text.read()
+            text.close()
+            return render_template('result.html', image1 = 'elbow.png', image2 = 'kmeans_clusters.png',text=content)
+        return 'Nothing Selected'
+        
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('algo.html', filename = filename)
     return render_template('upload.html')
 
-# @app.route('/predict', methods=['GET','POST'])
-# def predict():
-	
-# 	# Receives the input query from form
-# 	if request.method == 'POST':
-# 		namequery = request.form['namequery']
-#         # model=request.form['modelname']
-#         # modelload=open("models/"+str(model)+".pkl","rb")
-#         # clf = joblib.load(modelload)
-# 		data = [namequery]
-# 		vect = cv.transform(data).toarray()
-# 		my_prediction = clf.predict(vect)
-# 	return render_template('results.html',prediction = my_prediction,name = namequery.upper())
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
